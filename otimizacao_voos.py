cb8174a794cd347e149ce0d3f687db085113bdcf
fabio.kuhn <fabio.kuhn@el.com.br>
2020-03-18 19:59:14 -0300

    começo

---
+++ otimizacao_voos.py
@@ -0,0 +1,12 @@
+import time
+import random
+import math
+
+pessoas = [('Amanada', 'CWB'),
+           ('Pedro', 'GIG'),
+           ('Marcos', 'POA'),
+           ('Priscila', 'FLN'),
+           ('Jessica', 'ONF'),
+           ('Paulo', 'GYN')]
+
+destino = 'GRU'
